9507eb2d7260f482750f1a00ed8dc794fe87c1f1
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-14 05:43:33 +0530

    fix: updating the new way of defining the BaseModel and removing unnecessary imports

--- model/models.py
+++ model/models.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any, Union
+from typing import List, Annotated
 
 
 class Metadata(BaseModel):
-    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Summary: Annotated[List[str], Field(default_factory=list, description="Summary of the document")]
     Title: str
     Author: str
     DateCreated: str   
     LastModifiedDate: str
     Publisher: str
     Language: str
-    PageCount: Union[int, str]  # Can be "Not Available"
+    PageCount: int | str  # Can be "Not Available"
     SentimentTone: str
